6ab58d86246d2c678b2f0bca18d51383935ad8ca
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-01-01 23:29:06 -0500

    Edits for the implementation of the fgame command

--- botutils/GameChooser.py
+++ botutils/GameChooser.py
@@ -1,15 +1,22 @@
 """Contains the GameChooser class"""
 
-class GameChooser:
-    """A class to faciliate gamemode choosing and voting"""
+from botc.Game import Game
 
-    from botc.Game import Game
-    
-    _default_game = Game()
+class GameChooser:
+    """A class to facilitate gamemode choosing and voting"""
+
+    def __init__(self):
+        self._default_game = Game()
+        self._selected_game = Game()
 
     @property
     def default_game(self):
         return self._default_game
-    
-    def get_selected_game(self):
-        return self._default_game
+
+    @property
+    def selected_game(self):
+        return self._selected_game
+
+    @selected_game.setter
+    def selected_game(self, newly_selected_game):
+        self._selected_game = newly_selected_game
