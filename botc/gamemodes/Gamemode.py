a24518d538c33f0e8d5adb03c1f41acf105d5f63
Picowchew <58180935+Picowchew@users.noreply.github.com>
2021-01-01 23:28:01 -0500

    Removed hyphens, which would alter the logs (and other bot text), but would subjectively be more aesthetically pleasing

--- botc/gamemodes/Gamemode.py
+++ botc/gamemodes/Gamemode.py
@@ -5,6 +5,6 @@ import enum
 class Gamemode(enum.Enum):
     """BoTC gamemode enum class: TB, BMR, S&V"""
 
-    trouble_brewing = "Trouble-Brewing"
-    bad_moon_rising = "Bad-Moon-Rising"
-    sects_and_violets = "Sects-&-Violets"
+    trouble_brewing = "Trouble Brewing"
+    bad_moon_rising = "Bad Moon Rising"
+    sects_and_violets = "Sects & Violets"
